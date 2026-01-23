4d97a0b82aa73b8b1ea4514ac5c44b9f550b8a19
Sevy Harris <sevy.harris@gmail.com>
2026-01-23 10:51:52 -0500

    save sketch of module

---
+++ ess.py
@@ -0,0 +1,15 @@
+# Let's sketch out generally how this will go:
+
+# imports
+
+# load settings
+
+# set up the zeus sampler
+
+# run the zeus sampler
+
+# save files
+
+# do analysis
+
+# make plots
